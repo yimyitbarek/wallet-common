2822595cd6d0a7aa40b6c4d22e247815316da052
yitbarekyohannes <yitbarekyohannes0@gmail.com>
2026-02-22 10:08:03 +0100

    fix parser

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export * from './rendering';
 export * from './ParsingEngine';
 export * from './credential-parsers/SDJWTVCParser';
 export * from './credential-parsers/MsoMdocParser';
+export * from './credential-parsers/JWTVCParser';
 
 export * from './VerifyingEngine';
 export * from './credential-verifiers/SDJWTVCVerifier';
